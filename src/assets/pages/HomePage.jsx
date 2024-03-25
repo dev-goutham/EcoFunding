b9613104f2529814eeeb4c655f6ff155b7127524
LuigiHard <79230985+LuigiHard@users.noreply.github.com>
2024-03-25 13:35:42 -0300

    aaa

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import OqueFazemos from 'components/OqueFazemos';
-import SelosSection from 'components/Selos';
-import ESG from 'components/ESG';
-import Parceiros from 'components/Parceiros';
-import FAQ from 'components/FAQ';
-import Blog from 'components/Blog';
-import Rodape from 'components/Rodape';
-import HomeCarousel from 'components/Home';
+import OqueFazemos from '../../components/OqueFazemos';
+import SelosSection from '../../components/Selos';
+import ESG from '../../components/ESG';
+import Parceiros from '../../components/Parceiros';
+import FAQ from '../../components/FAQ';
+import Blog from '../../components/Blog';
+import Rodape from '../../components/Rodape';
+import HomeCarousel from '../../components/Home';
 import styled from 'styled-components';
-import { MarkerProvider } from 'components/earth/MarkerContext';
+
 import FormContainer from '../../containers/FormContainer';
 
 const RodapeWrapper = styled.div`
