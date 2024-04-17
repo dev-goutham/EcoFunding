eedf4291cfe65c5ae3733b21741330b4556437c9
LuigiHard <79230985+LuigiHard@users.noreply.github.com>
2024-04-16 21:32:18 -0300

    m

--- src/config/AuthContext.js
+++ src/config/AuthContext.js
@@ -9,15 +9,22 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState();
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, user => {
       setCurrentUser(user);
+      setLoading(false); // Set loading to false once authentication state is determined
     });
 
     return unsubscribe;
   }, []);
+
+  // Render loading spinner or placeholder until loading is complete
+  if (loading) {
+    return <h1> Carregando... </h1>; // Placeholder for loading
+  }
 
   return (
     <AuthContext.Provider value={{ currentUser }}>
