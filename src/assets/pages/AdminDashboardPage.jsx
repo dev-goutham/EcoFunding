7c9505f84140592a9bf4ba105f869d06130d5aab
LuigiHard <79230985+LuigiHard@users.noreply.github.com>
2024-04-26 15:21:36 -0300

    Remove all

--- src/assets/pages/AdminDashboardPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import AdminDashboard from "../../components/AdminDashboard/AdminDashboard.jsx";
-function AdminDashboardPage() {
-    return <AdminDashboard />;
-    }
-export default AdminDashboardPage;
--- src/vite.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  resolve: {
-    alias: {
-      "ui-components": "src/ui/components",
-    },
-  },
-};
